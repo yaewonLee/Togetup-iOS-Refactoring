fc8a50b6d4069a79dc0ffc02d28e3cf1c87a7985
Yaewon Lee <yw990324@gmail.com>
2023-08-14 19:46:52 +0900

    프로젝트 초기 세팅

---
+++ TogetUp/TogetUp/Screen/Alarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TogetUp
+//
+//  Created by 이예원 on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
