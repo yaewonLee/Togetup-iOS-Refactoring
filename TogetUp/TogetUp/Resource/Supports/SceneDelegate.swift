f8e85aadafde92ba3789167b75131db7432e60c4
Yaewon Lee <yw990324@gmail.com>
2023-08-18 19:43:04 +0900

    Feat: Apple 로그인 로직 추가

--- TogetUp/TogetUp/Resource/Supports/SceneDelegate.swift
+++ TogetUp/TogetUp/Resource/Supports/SceneDelegate.swift
@@ -8,9 +8,14 @@
 import UIKit
 import RxKakaoSDKAuth
 import KakaoSDKAuth
+import AuthenticationServices
+import RxSwift
+import KakaoSDKUser
+import RxKakaoSDKUser
+import KakaoSDKCommon
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    let disposeBag = DisposeBag()
     var window: UIWindow?
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
@@ -22,10 +27,66 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: KeyChainManager.shared.getUserIdentifier()!) { (credentialState, error) in
+                switch credentialState {
+                case .authorized:
+                    DispatchQueue.main.async {
+                        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                        let viewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+
+                        self.window?.rootViewController = viewController
+                        self.window?.makeKeyAndVisible()
+                    }
+                    
+                case .revoked, .notFound:
+                    DispatchQueue.main.async {
+                        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                        let loginNavigationController = mainStoryboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+
+                        self.window?.rootViewController = loginNavigationController
+                        self.window?.makeKeyAndVisible()
+                    }
+
+                default:
+                    print(error?.localizedDescription)
+                }
+            }
+        if (AuthApi.hasToken()) {
+            UserApi.shared.rx.accessTokenInfo()
+                .subscribe(onSuccess:{ (_) in
+                    DispatchQueue.main.async {
+                        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                        let viewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+
+                        self.window?.rootViewController = viewController
+                        self.window?.makeKeyAndVisible()
+                    }
+                }, onFailure: {error in
+                    if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true  {
+                        DispatchQueue.main.async {
+                            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                            let loginNavigationController = mainStoryboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+
+                            self.window?.rootViewController = loginNavigationController
+                            self.window?.makeKeyAndVisible()
+                        }
+                    }
+                    else {
+                        print(error.localizedDescription)
+                    }
+                })
+                .disposed(by: disposeBag)
+        }
+        else {
+            DispatchQueue.main.async {
+                let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let loginNavigationController = mainStoryboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+
+                self.window?.rootViewController = loginNavigationController
+                self.window?.makeKeyAndVisible()
+            }
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -43,11 +104,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
--- TogetUp/TogetUp/Screen/Login/View/LoginViewController.swift
+++ TogetUp/TogetUp/Screen/Login/View/LoginViewController.swift
@@ -9,35 +9,73 @@ import UIKit
 import RxSwift
 import KakaoSDKUser
 import RxKakaoSDKUser
+import AuthenticationServices
+import KeychainAccess
 
-class LoginViewController: UIViewController {
-    // MARK: - Properties
+class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
     let disposeBag = DisposeBag()
     
-    // MARK: - UI Components
-
-    @IBOutlet weak var kakaoLoginButton: UIButton!
-    
-    
-    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let authorizationCode = appleIDCredential.authorizationCode
+            let identityToken = appleIDCredential.identityToken
+            let userIdentifier = appleIDCredential.user
+
+            KeyChainManager.shared.saveUserIdentifier(userIdentifier)
+            switchView()
+
+            print("authorizationCode: \(authorizationCode)")
+            print("identityToken: \(identityToken)")
+            print("user: \(userIdentifier)")
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Apple login error: \(error)")
+    }
+    
+    private func switchView() {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") else {
+            return
+        }
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+    
+    
+    
     // MARK: - @IBAction
+    @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
     @IBAction func kakaoLoginButtontapped(_ sender: UIButton) {
         if (UserApi.isKakaoTalkLoginAvailable()) {
             UserApi.shared.rx.loginWithKakaoTalk()
                 .subscribe(onNext:{ (oauthToken) in
                     print("loginWithKakaoTalk() success.")
                     print(oauthToken)
+                    self.switchView()
                 }, onError: {error in
-                    print(error)
+                    print(error.localizedDescription)
+                    print("카카오톡 설치 필요")
                 })
-            .disposed(by: disposeBag)
+                .disposed(by: disposeBag)
         }
     }
-    
-
 }
