61f198e787adef56eac6d1a05dbd046aba8e7e30
Yaewon Lee <yw990324@gmail.com>
2023-09-03 19:59:02 +0900

    Feat: Setting 화면 UI 추가

--- TogetUp/TogetUp/Screen/Setting/View/SettingViewController.swift
+++ TogetUp/TogetUp/Screen/Setting/View/SettingViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var emptyView: UIView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var withdrawlButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        customUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func customUI() {
+        emptyView.layer.cornerRadius = 12
+        emptyView.layer.borderWidth = 2
+        emptyView.clipsToBounds = true
+        
+        logoutButton.layer.cornerRadius = 12
+        withdrawlButton.layer.cornerRadius = 12
     }
-    */
+    
 
 }
