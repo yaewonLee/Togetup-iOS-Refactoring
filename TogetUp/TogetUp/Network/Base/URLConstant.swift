9c162de96602b3566f26c2af49e28a2a3a106ce1
Yaewon Lee <yw990324@gmail.com>
2023-09-03 17:19:06 +0900

    Feat: URLConstant 추가

---
+++ TogetUp/TogetUp/Network/Base/URLConstant.swift
@@ -0,0 +1,23 @@
+//
+//  URLConstant.swift
+//  TogetUp
+//
+//  Created by 이예원 on 2023/08/31.
+//
+
+import Foundation
+
+struct URLConstant {
+    // MARK: - Base URL
+    static let baseURL = "https://togetup.shop"
+    
+    // MARK: - Auth 로그인
+    static let login = "/app/auth/login"
+    
+    // MARK: - 알람
+    static let getAlarmList = "/app/alarm"
+    static let createAlarm = "/app/alarm"
+    static let deleteAlarm = "/app/alarm/{alarmId}"
+    static let editAlarm = "/app/alarm/{alarmId}"
+
+}
