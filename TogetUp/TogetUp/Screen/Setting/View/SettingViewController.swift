d1fee61a36f95175a18dc47085841ca30220bc01
Yaewon Lee <yw990324@gmail.com>
2023-09-03 23:11:22 +0900

    Feat: UIAlert 추가

--- TogetUp/TogetUp/Screen/Setting/View/SettingViewController.swift
+++ TogetUp/TogetUp/Screen/Setting/View/SettingViewController.swift
@@ -40,35 +40,49 @@ class SettingViewController: UIViewController {
     }
     
     @IBAction func logout(_ sender: Any) {
-        if UserDefaults.standard.string(forKey: "loginMethod") == "Kakao" {
-            UserApi.shared.rx.logout()
-                .subscribe(onCompleted:{
-                    print("logout() success.")
-                    self.switchView()
-                }, onError: {error in
-                    print(error.localizedDescription)
-                })
-                .disposed(by: disposeBag)
+        let sheet = UIAlertController(title: "알림", message: "로그아웃하시겠습니까?", preferredStyle: .alert)
+        sheet.addAction(UIAlertAction(title: "취소", style: .default, handler: nil))
+        let okAction = UIAlertAction(title: "로그아웃", style: .destructive) { _ in
+            if UserDefaults.standard.string(forKey: "loginMethod") == "Kakao" {
+                UserApi.shared.rx.logout()
+                    .subscribe(onCompleted:{
+                        print("logout() success.")
+                        self.switchView()
+                    }, onError: { error in
+                        print(error.localizedDescription)
+                    })
+                    .disposed(by: self.disposeBag)
+            }
         }
+        sheet.addAction(okAction)
+        present(sheet, animated: true)
     }
     
     @IBAction func withdrawl(_ sender: Any) {
-        UserApi.shared.rx.unlink()
-            .subscribe(onCompleted: { [weak self] in
-                print("unlink() success.")
-                self?.viewModel.deleteUser()
-                    .subscribe(onNext: { [weak self] response in
-                        print("User deleted successfully on our server.")
-                        print(response.message)
-                        if response.httpStatusCode == 200 {
-                            self?.switchView()
-                        }
-                    }, onError: { error in
-                        print("Failed to delete user on our server:", error)
-                    }).disposed(by: self?.disposeBag ?? DisposeBag())
-            }, onError: { error in
-                print(error.localizedDescription)
-            })
-            .disposed(by: disposeBag)
+        let sheet = UIAlertController(title: "알림", message: "탈퇴하시겠습니까?", preferredStyle: .alert)
+        sheet.addAction(UIAlertAction(title: "취소", style: .default, handler: nil))
+        let okAction = UIAlertAction(title: "탈퇴하기", style: .destructive) { _ in
+            UserApi.shared.rx.unlink()
+                .subscribe(onCompleted: { [weak self] in
+                    print("unlink() success.")
+                    self?.viewModel.deleteUser()
+                        .subscribe(onNext: { [weak self] response in
+                            print("User deleted successfully on our server.")
+                            print(response.message)
+                            if response.httpStatusCode == 200 {
+                                self?.switchView()
+                            }
+                        }, onError: { error in
+                            print("Failed to delete user on our server:", error)
+                        }).disposed(by: self?.disposeBag ?? DisposeBag())
+                }, onError: { error in
+                    print(error.localizedDescription)
+                })
+                .disposed(by: self.disposeBag)
+        }
+        sheet.addAction(okAction)
+        present(sheet, animated: true)
     }
+  
 }
+
