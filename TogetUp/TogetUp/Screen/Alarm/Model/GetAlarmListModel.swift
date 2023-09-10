0358fdee0c1d053040b316f78f8c0b7b032c94d3
Yaewon Lee <yw990324@gmail.com>
2023-09-10 20:44:33 +0900

    Feat: GetAlarmListRequest 데이터 모델 생성

---
+++ TogetUp/TogetUp/Screen/Alarm/Model/GetAlarmListModel.swift
@@ -0,0 +1,41 @@
+//
+//  GetAlarmListModel.swift
+//  TogetUp
+//
+//  Created by 이예원 on 2023/09/10.
+//
+
+import Foundation
+
+struct GetAlarmListRequest: Codable {
+    let id: Int
+    let userId: Int
+    let name: String
+    let icon: String
+    let snoozeInterval: Int
+    let snoozeCnt: Int
+    let alarmTime: String
+    let monday, tuesday, wednesday, thursday, friday, saturday, sunday: Bool
+    let isSnoozeActivated, isVibrate, isActivated: Bool
+    let getMissionRes: MissionRes?
+    let getMissionObjectRes: MissionObjectRes?
+    let roomRes: RoomRes?
+}
+
+struct MissionRes: Codable {
+    let id: Int
+    let name: String
+    let createdAt: String
+    let isActive: Bool
+}
+
+struct MissionObjectRes: Codable {
+    let id: Int
+    let name, kr, icon: String
+}
+
+struct RoomRes: Codable{
+    let id: Int
+    let name, intro, groupProfileImgLink, password: String
+    let state: Int
+}
