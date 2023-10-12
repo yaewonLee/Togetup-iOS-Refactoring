d42738ec7a710e78594a318906cae39b0f765961
Yaewon Lee <yw990324@gmail.com>
2023-10-12 21:53:07 +0900

    Feat: 그룹 리스트 컬렉션뷰 바인딩

--- TogetUp/TogetUp/Screen/Group/View/GroupListViewController.swift
+++ TogetUp/TogetUp/Screen/Group/View/GroupListViewController.swift
@@ -6,11 +6,53 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class GroupListViewController: UIViewController {
-
+    // MARK: - UI Components
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Properties
+    private let viewModel = GroupListViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setCollectionViewFlowLayout()
+        setCollectionView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Custom Method
+    private func setCollectionView() {
+        collectionView.delegate = nil
+        collectionView.dataSource = nil
+        
+        viewModel.loginReqeust()
+            .map { $0.result.compactMap { $0 } }
+            .bind(to: collectionView.rx.items(cellIdentifier: GroupListCollectionViewCell.identifier,
+                                              cellType: GroupListCollectionViewCell.self)) { index, model, cell in
+                cell.setAttributes(with: model)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    private func customUI() {
+        
+    }
+    
+    private func setCollectionViewFlowLayout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: collectionView.bounds.width - 40, height: 68)
+        layout.minimumLineSpacing = 16
+        collectionView.collectionViewLayout = layout
+    }
+    // MARK: - @
+    
 }
