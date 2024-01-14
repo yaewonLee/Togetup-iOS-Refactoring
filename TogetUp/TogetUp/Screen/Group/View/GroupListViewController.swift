4960d54020df37b7538776cdfb78b2a8232cb96e
Yaewon Lee <yw990324@gmail.com>
2024-01-14 16:18:04 +0900

    Feat: 팝업창 추가

--- TogetUp/TogetUp/Screen/Group/View/GroupListViewController.swift
+++ TogetUp/TogetUp/Screen/Group/View/GroupListViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class GroupListViewController: UIViewController {
+class GroupListViewController: UIViewController, UIGestureRecognizerDelegate, UITextFieldDelegate {
     // MARK: - UI Components
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var inviteCodeView: UIView!
+    @IBOutlet weak var inviteTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
     
     // MARK: - Properties
     private let viewModel = GroupListViewModel()
@@ -22,7 +26,10 @@ class GroupListViewController: UIViewController {
         super.viewDidLoad()
         setCollectionViewFlowLayout()
         setCollectionView()
-      //  self.performSegue(withIdentifier: "toGroupBoardSegue", sender: nil)
+        customUI()
+        setTextFieldGesture()
+        
+        //  self.performSegue(withIdentifier: "toGroupBoardSegue", sender: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,6 +38,17 @@ class GroupListViewController: UIViewController {
     }
     
     // MARK: - Custom Method
+    private func customUI() {
+        inviteCodeView.layer.cornerRadius = 16
+        inviteCodeView.layer.borderWidth = 2
+        inviteTextField.layer.cornerRadius = 12
+        inviteTextField.layer.borderWidth = 2
+        cancelButton.layer.cornerRadius = 20
+        cancelButton.layer.borderWidth = 2
+        okButton.layer.cornerRadius = 20
+        okButton.layer.borderWidth = 2
+    }
+    
     private func setCollectionView() {
         collectionView.delegate = nil
         collectionView.dataSource = nil
@@ -41,7 +59,7 @@ class GroupListViewController: UIViewController {
                                               cellType: GroupListCollectionViewCell.self)) { index, model, cell in
                 cell.setAttributes(with: model)
             }
-                                              .disposed(by: disposeBag)
+              .disposed(by: disposeBag)
         
         collectionView.rx.itemSelected
             .subscribe(onNext: { [weak self] indexPath in
@@ -52,6 +70,16 @@ class GroupListViewController: UIViewController {
                 }
             })
             .disposed(by: disposeBag)
+    }
+    
+    private func setTextFieldGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,5 +93,24 @@ class GroupListViewController: UIViewController {
         layout.itemSize = CGSize(width: collectionView.bounds.width - 40, height: 68)
         layout.minimumLineSpacing = 16
         collectionView.collectionViewLayout = layout
+    }
+    
+    // MARK: - @
+    @objc func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func inviteButton(_ sender: Any) {
+        inviteCodeView.isHidden = false
+    }
+    
+    @IBAction func cancelInviteCodeButton(_ sender: Any) {
+        inviteCodeView.isHidden = true
+    }
+    
+    @IBAction func okButtonTapped(_ sender: Any) {
+        let code = inviteTextField.text
+        guard let vc = storyboard?.instantiateViewController(identifier: "GroupJoinViewController") as? GroupJoinViewController else { return }
+        vc.code = code ?? ""
     }
 }
