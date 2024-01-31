cba8c7bde5e222a788c2bd95219708a83e17cd18
Yaewon Lee <yw990324@gmail.com>
2024-01-31 23:23:50 +0900

    Refactor: 네트워킹, realm 관련 코드 분리

--- TogetUp/TogetUp/Screen/Alarm/ViewModel/EditAlarmViewModel.swift
+++ TogetUp/TogetUp/Screen/Alarm/ViewModel/EditAlarmViewModel.swift
@@ -10,135 +10,65 @@ import RxSwift
 import Moya
 import RealmSwift
 
-enum CreateAlarmError: Error {
-    case network(MoyaError)
-    case server(Int)
-}
-
 class EditAlarmViewModel {
     private let provider = MoyaProvider<AlarmService>()
+    private let realmManager = AlarmDataManager()
+    private let networkManager = NetworkManager()
+    private let disposeBag = DisposeBag()
     
-//    init(provider: MoyaProvider<AlarmService> = MoyaProvider<AlarmService>(plugins: [NetworkLogger()])) {
-//            self.provider = provider
-//        }
-
-    // MARK: - API Call Methods
-    func handleAPIRequest<T: Decodable>(_ request: Single<Response>) -> Single<Result<T, CreateAlarmError>> {
-        return request
-            .filterSuccessfulStatusAndRedirectCodes()
-            .map(T.self)
-            .map(Result.success)
-            .catch { error -> Single<Result<T, CreateAlarmError>> in
-                if let moyaError = error as? MoyaError {
-                    switch moyaError {
-                    case .statusCode(let response):
-                        return Single.just(.failure(.server(response.statusCode)))
-                    default:
-                        return Single.just(.failure(.network(moyaError)))
-                    }
-                } else {
-                    return Single.just(.failure(.network(MoyaError.underlying(error, nil))))
+    var errorMessage = PublishSubject<String>()
+    
+    func getSingleAlarm(alarmId: Int) -> Single<GetSingleAlarmResponse> {
+        return networkManager.handleAPIRequest(provider.rx.request(.getSingleAlarm(alarmId: alarmId)), dataType: GetSingleAlarmResponse.self)
+            .flatMap { result -> Single<GetSingleAlarmResponse> in
+                switch result {
+                case .success(let response):
+                    return .just(response)
+                case .failure(let error):
+                    let message = self.networkManager.errorMessage(for: error)
+                    self.errorMessage.onNext(message)
+                    return .error(error)
                 }
             }
     }
-
-    func getSingleAlarm(id: Int) -> Single<Result<GetSingleAlarmResponse, CreateAlarmError>> {
-        return handleAPIRequest(provider.rx.request(.getSingleAlarm(alarmId: id)))
-    }
-
-    func postAlarm(param: CreateOrEditAlarmRequest) -> Single<Result<CreateEditDeleteAlarmResponse, CreateAlarmError>> {
-        return handleAPIRequest(provider.rx.request(.createAlarm(param: param)))
-    }
-
-    func editAlarm(alarmId: Int, param: CreateOrEditAlarmRequest) -> Single<Result<CreateEditDeleteAlarmResponse, CreateAlarmError>> {
-        return handleAPIRequest(provider.rx.request(.editAlarm(alarmId: alarmId, param: param)))
-    }
-
-    func deleteAlarm(alarmId: Int) -> Single<Result<Int, CreateAlarmError>> {
-        return provider.rx.request(.deleteAlarm(alarmId: alarmId))
-            .filterSuccessfulStatusAndRedirectCodes()
-            .map(CreateEditDeleteAlarmResponse.self)
-            .flatMap { response -> Single<Result<Int, CreateAlarmError>> in
-                if let resultId = response.result, resultId == alarmId {
-                    let realmInstance = try! Realm()
-                    if let alarmToDelete = realmInstance.objects(Alarm.self).filter("id == \(alarmId)").first {
-                        do {
-                            AlarmScheduleManager.shared.removeNotification(for: alarmId)
-                            try realmInstance.write {
-                                realmInstance.delete(alarmToDelete)
-                            }
-                            return Single.just(.success(resultId))
-                        } catch {
-                            print("Error deleting alarm from Realm")
-                            return Single.just(.failure(.server(500)))
-                        }
-                    } else {
-                        print("Alarm not found in Realm")
-                        return Single.just(.failure(.server(404)))
-                    }
-                } else {
-                    return Single.just(.failure(.server(response.httpStatusCode)))
-                }
-            }
-            .catch { error in
-                if let moyaError = error as? MoyaError {
-                    switch moyaError {
-                    case .statusCode(let response):
-                        print("Status code: \(response.statusCode)")
-                        return Single.just(.failure(.server(response.statusCode)))
-                    default:
-                        print("Other error: \(moyaError.localizedDescription)")
-                        return Single.just(.failure(.network(moyaError)))
-                    }
-                } else {
-                    print("Unknown error: \(error)")
-                    return Single.just(.failure(.network(MoyaError.underlying(error, nil))))
+    
+    func postAlarm(param: CreateOrEditAlarmRequest, missionEndpoint: String) -> Single<Result<Void, Error>> {
+        return networkManager.handleAPIRequest(provider.rx.request(.createAlarm(param: param)), dataType: CreateEditDeleteAlarmResponse.self)
+            .flatMap { [weak self] result -> Single<Result<Void, Error>> in
+                switch result {
+                case .success(let response):
+                    let alarmId = response.result
+                    self?.realmManager.updateAlarm(with: param, for: alarmId ?? 0, missionEndpoint: missionEndpoint)
+                    return .just(.success(()))
+                case .failure(let error):
+                    return .just(.failure(error))
                 }
             }
     }
-
-    // MARK: - Realm Methods
-    func saveOrUpdateAlarmInRealm(id: Int, missionId: Int, missionObjectId: Int, isSnoozeActivated: Bool, name: String, icon: String, isVibrate: Bool, alarmHour: Int, alarmMinute: Int, days: [Bool], isActivated: Bool, missionName: String, missionEndpoint: String) {
-        let realmInstance = try! Realm()
-
-        if let alarm = realmInstance.objects(Alarm.self).filter("id == \(id)").first {            
-            try? realmInstance.write {
-                updateAlarmFields(alarm: alarm, missionId: missionId, missionObjectId: missionObjectId, isSnoozeActivated: isSnoozeActivated, name: name, icon: icon, isVibrate: isVibrate, alarmHour: alarmHour, alarmMinute: alarmMinute, days: days, isActivated: isActivated, missionName: missionName, missionEndpoint: missionEndpoint)
+    
+    func editAlarm(param: CreateOrEditAlarmRequest, missionEndpoint: String, alarmId: Int) -> Single<Result<Void, Error>> {
+        return networkManager.handleAPIRequest(provider.rx.request(.editAlarm(alarmId: alarmId, param: param)), dataType: CreateEditDeleteAlarmResponse.self)
+            .flatMap { [weak self] result -> Single<Result<Void, Error>> in
+                switch result {
+                case .success:
+                    self?.realmManager.updateAlarm(with: param, for: alarmId, missionEndpoint: missionEndpoint)
+                    return .just(.success(()))
+                case .failure(let error):
+                    return .just(.failure(error))
+                }
             }
-            AlarmScheduleManager.shared.toggleAlarmActivation(for: id)
-        } else {
-            let newAlarm = Alarm()
-            newAlarm.id = id
-            updateAlarmFields(alarm: newAlarm, missionId: missionId, missionObjectId: missionObjectId, isSnoozeActivated: isSnoozeActivated, name: name, icon: icon, isVibrate: isVibrate, alarmHour: alarmHour, alarmMinute: alarmMinute, days: days, isActivated: isActivated, missionName: missionName, missionEndpoint: missionEndpoint)
-            try? realmInstance.write {
-                realmInstance.add(newAlarm, update: .modified)
-            }
-            AlarmScheduleManager.shared.scheduleNotification(for: id)
-        }
     }
-
-    private func updateAlarmFields(alarm: Alarm, missionId: Int, missionObjectId: Int, isSnoozeActivated: Bool, name: String, icon: String, isVibrate: Bool, alarmHour: Int, alarmMinute: Int, days: [Bool], isActivated: Bool, missionName: String, missionEndpoint: String) {
-        alarm.missionId = missionId
-        alarm.missionObjectId = missionObjectId
-        alarm.isSnoozeActivated = isSnoozeActivated
-        alarm.name = name
-        alarm.icon = icon
-        alarm.isVibrate = isVibrate
-        alarm.alarmHour = alarmHour
-        alarm.alarmMinute = alarmMinute
-        alarm.monday = days[0]
-        alarm.tuesday = days[1]
-        alarm.wednesday = days[2]
-        alarm.thursday = days[3]
-        alarm.friday = days[4]
-        alarm.saturday = days[5]
-        alarm.sunday = days[6]
-        alarm.isActivated = isActivated
-        alarm.missionName = missionName
-        alarm.missionEndpoint = missionEndpoint
+    
+    func deleteAlarm(alarmId: Int) -> Single<Result<Void, Error>> {
+        return networkManager.handleAPIRequest(provider.rx.request(.deleteAlarm(alarmId: alarmId)), dataType: CreateEditDeleteAlarmResponse.self)
+            .flatMap { [weak self] result -> Single<Result<Void, Error>> in
+                switch result {
+                case .success:
+                    self?.realmManager.deleteAlarm(alarmId: alarmId)
+                    return .just(.success(()))
+                case .failure(let error):
+                    return .just(.failure(error))
+                }
+            }
     }
 }
-
-
-
-
