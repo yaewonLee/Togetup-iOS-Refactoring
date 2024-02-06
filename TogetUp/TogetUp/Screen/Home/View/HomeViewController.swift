2fd640bcf448f1bb007cf4bf44469b4f651896ec
Yaewon Lee <yw990324@gmail.com>
2024-02-06 15:09:39 +0900

    Refactor:  CustomFloatingPanelLayout 클래스 파일로 분리

--- TogetUp/TogetUp/Screen/Home/View/HomeViewController.swift
+++ TogetUp/TogetUp/Screen/Home/View/HomeViewController.swift
@@ -182,27 +182,3 @@ extension HomeViewController: UICollectionViewDelegate {
         selectedIndex = indexPath
     }
 }
-
-class CustomFloatingPanelLayout: FloatingPanelLayout {
-    var position: FloatingPanelPosition {
-        return .bottom
-    }
-    
-    var initialState: FloatingPanelState {
-        return .tip
-    }
-    
-    var anchors: [FloatingPanelState : FloatingPanelLayoutAnchoring] {
-        return [
-            .full: FloatingPanelLayoutAnchor(absoluteInset: 80,  edge: .top, referenceGuide: .safeArea),
-            .half: FloatingPanelLayoutAnchor(absoluteInset: 175, edge: .bottom, referenceGuide: .safeArea),
-            .tip: FloatingPanelLayoutAnchor(absoluteInset: 175, edge: .bottom, referenceGuide: .safeArea)
-        ]
-    }
-    
-    func backdropAlpha(for state: FloatingPanelState) -> CGFloat {
-        return state == .full ? 0.3 : 0.0
-    }
-}
-
-
