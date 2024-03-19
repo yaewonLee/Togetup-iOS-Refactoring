25c90117f16cd737db853048b2716f6e751c1385
Yaewon Lee <yw990324@gmail.com>
2024-03-20 01:12:13 +0900

    Feat: 미션 수행 후 로컬 데이터 업데이트 로직 추가

--- TogetUp/TogetUp/Screen/Mission/ViewModel/MissionProcessViewModel.swift
+++ TogetUp/TogetUp/Screen/Mission/ViewModel/MissionProcessViewModel.swift
@@ -13,16 +13,36 @@ import UIKit
 
 class MissionProcessViewModel {
     private let provider: MoyaProvider<MissionService>
+    private let networkManager = NetworkManager()
+    private let disposeBag = DisposeBag()
     
     init(provider: MoyaProvider<MissionService> = MoyaProvider<MissionService>(plugins: [NetworkLogger()])) {
-            self.provider = provider
-        }
+        self.provider = provider
+    }
     
     func sendMissionImage(objectName: String, missionImage: UIImage) -> Observable<MissionDetectResponse> {
         return provider.rx.request(.missionDetection(objectName: objectName, missionImage: missionImage))
             .filterSuccessfulStatusCodes()
             .map(MissionDetectResponse.self)
             .asObservable()
+    }
+    
+    func completeMission(param: MissionCompleteRequest, completion: @escaping (Result<MissionCompleteResponse, Error>) -> Void) {
+        networkManager.handleAPIRequest(provider.rx.request(.missionComplete(param: param)), dataType: MissionCompleteResponse.self)
+            .subscribe(onSuccess: { result in
+                switch result {
+                case .success(let response):
+                    completion(.success(response))
+                    if let userResult = response.result?.userStat {
+                        UserDataManager.shared.updateUserStatus(level: userResult.level, expPercentage: userResult.expPercentage)
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }, onFailure: { error in
+                completion(.failure(error))
+            })
+            .disposed(by: disposeBag)
     }
 }
 
