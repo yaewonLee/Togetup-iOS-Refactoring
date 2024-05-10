6020958d77c31f43177ab23fc37754542a652980
Yaewon Lee <yw990324@gmail.com>
2024-05-10 22:42:42 +0900

    Feat: Webkit ViewController 추가

---
+++ TogetUp/TogetUp/Screen/Setting/View/WebkitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebkitViewController.swift
+//  TogetUp
+//
+//  Created by 이예원 on 5/10/24.
+//
+
+import UIKit
+import WebKit
+
+class WebkitViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    var url = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
